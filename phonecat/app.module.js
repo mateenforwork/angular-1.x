50cc0b96ae275cdec6692d24a2f9d12652511ce8
Mateen <mateenforwork@gmail.com>
2018-09-17 21:21:03 +0530

    Update app.module.js

--- phonecat/app.module.js
+++ phonecat/app.module.js
@@ -1,3 +1,5 @@
+document.writeln('<script src="phone-list/phone-list.module.js"></script>');
+document.writeln('<script src="phone-list/phone-list.component.js"></script>');
 // Define the `phonecatApp` module
 angular.module('phonecatApp', [
     // ...which depends on the `phoneList` module
